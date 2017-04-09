d4903cc7acdbf04e1ffffa3464ef0afe49a3f30a
Your Name <christle.lee86@gmail.com>
2017-04-10 08:40:19 +1000

    responsiveness

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
-  // Parallax effect
-  var $body = $('body');
-  var $banner = $('.banner');
-  $(window).on('scroll', function () {
-    var scrollTop = $(window).scrollTop();
-    $body.css('background-position-y', -scrollTop / 2); // Entire background
-    $banner.css('background-position-y', -scrollTop * 0.75); 
-  });
+  // // Parallax effect
+  // var $body = $('body');
+  // var $banner = $('.banner');
+  // $(window).on('scroll', function () {
+  //   var scrollTop = $(window).scrollTop();
+  //   $body.css('background-position-y', -scrollTop / 2); // Entire background
+  //   $banner.css('background-position-y', -scrollTop * 0.75);
+  // });
 
   //map
+  $("#NSW").addClass('active');
+
   $( "#QLD" ).on("click", function() {
     $(this).addClass('active').siblings().removeClass('active');
     $('.bris').show().siblings().hide('bris');
